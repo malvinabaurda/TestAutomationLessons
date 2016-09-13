c2fb2b34e74b09bf3934928d6df26a5eea563753
mukoyidserg <mukoyidserg@gmail.com>
2016-09-13 12:46:24 +0300

    added comment to the main method

--- src/Dinnerware.java
+++ src/Dinnerware.java
@@ -21,7 +21,7 @@ public class Dinnerware {
 	public String getNameOfOwner() {
 		return nameOfOwner;
 	}
-
+//This is comment for the main method
 	public static void main(String[] args) {
 		// Creation of class instances
 		Dinnerware malvinaDinnerware = new Dinnerware();
