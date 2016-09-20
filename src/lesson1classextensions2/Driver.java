4d1c68a907e56e6b60426a088ddbe748c49592ce
Administrator <Administrator@WIN-RQBMLHMFIDP.d-net.kiev.ua>
2016-09-20 17:00:15 +0300

    lesson1classextensions2 creation

---
+++ src/lesson1classextensions2/Driver.java
@@ -0,0 +1,23 @@
+package lesson1classextensions2;
+
+public class Driver extends Human {
+	private int drivingExperiance;
+	private String car;
+
+	public int getDrivingExperiance() {
+		return drivingExperiance;
+	}
+
+	public void setDrivingExperiance(int drivingExperiance) {
+		this.drivingExperiance = drivingExperiance;
+	}
+
+	public String getCar() {
+		return car;
+	}
+
+	public void setCar(String car) {
+		this.car = car;
+	}
+
+}
---
+++ src/lesson1classextensions2/Race.java
@@ -0,0 +1,13 @@
+package lesson1classextensions2;
+
+public class Race {
+
+	public static void main(String[] args) {
+	
+		Driver driverOne = new Driver();
+		driverOne.setName("Liza");
+		System.out.println(driverOne.getName());
+	
+	}
+
+}
